f72736d624928867755dd2c1bcffc1a37a8d5fa3
Parce <marco.diaz.garcia@mni.thm.com>
2024-02-21 13:30:21 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'boulder_cam',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
